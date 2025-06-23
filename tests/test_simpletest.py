fa0511c612fb7f4df659dbd7f331ebc5cae2ee7b
baggiiiie <yc@yingchaoooo.com>
2025-06-23 15:26:35 +0800

    feat: test cases

--- tests/test_simpletest.py
+++ tests/test_simpletest.py
@@ -1,11 +1,15 @@
 import pytest
+import allure
 from src.case_runner import case_runner as runner
+from src.utils.case_utils import load_test_cases
 
 
-test_cases = runner.load_test_cases_from_json("tests/simpletest.json")
+test_suites = load_test_cases("tests/simpletest.json")
 
 
+@allure.feature(test_suites.description)
 class TestSimpleTestOne:
-    @pytest.mark.parametrize("test_case", test_cases)
+    @pytest.mark.parametrize("test_case", test_suites.test_cases)
     def test_my_func(self, test_case):
+        # print("runing test case:", test_case.description)
         runner.execute_test_case(test_case)
