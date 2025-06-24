37dbf8da9ce0a36110b41fdd4c4d9e93e87eb14b
baggiiiie <yc@yingchaoooo.com>
2025-06-24 15:55:44 +0800

    feat: add curlify

--- src/api_clients/simple_client.py
+++ src/api_clients/simple_client.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import allure
+from typing import Dict
+import curlify
+
 
 PROTOCOL = "http"
 HOST = "localhost"
@@ -11,29 +14,44 @@ BASE_URL = f"{PROTOCOL}://{HOST}:{PORT}"
 class HttpClient:
     def __init__(self, base_url=BASE_URL):
         self.base_url = base_url.rstrip("/")
+        self.headers = {}
+        self.set_jwt()  # Automatically set JWT on initialization
+
+    def set_header(self, content: Dict):
+        for key, value in content.items():
+            self.headers[key] = value
+
+    def set_jwt(self):
+        # mock setting jwt
+        JWT = "abcd1234efgh5678ijkl9012mnop3456qrst7890uvwx"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {JWT}",
+        }
+        self.set_header(headers)
 
     def _attach_allure(self, method, url, request_body, request_headers, response):
         with allure.step(f"{method.upper()} {url}"):
-            if request_body:
-                allure.attach(
-                    json.dumps(request_body, indent=2),
-                    name="HTTP Request Body",
-                    attachment_type=allure.attachment_type.JSON,
-                )
-
-            if request_headers:
-                allure.attach(
-                    json.dumps(dict(request_headers), indent=2),
-                    name="HTTP Request Headers",
-                    attachment_type=allure.attachment_type.JSON,
-                )
-
+            allure.attach(
+                json.dumps(dict(request_headers), indent=2),
+                name="HTTP Request Headers",
+                attachment_type=allure.attachment_type.JSON,
+            )
+            allure.attach(
+                json.dumps(request_body, indent=2),
+                name="HTTP Request Body",
+                attachment_type=allure.attachment_type.JSON,
+            )
             allure.attach(
                 json.dumps(dict(response.headers), indent=2),
                 name="Response Headers",
                 attachment_type=allure.attachment_type.JSON,
             )
-
+            allure.attach(
+                str(response.status_code),
+                name="HTTP Status Code",
+                attachment_type=allure.attachment_type.TEXT,
+            )
             try:
                 response_body = response.json()
                 body_text = json.dumps(response_body, indent=2)
@@ -41,14 +59,13 @@ class HttpClient:
             except Exception:
                 body_text = response.text
                 attachment_type = allure.attachment_type.TEXT
-
-            allure.attach(
-                response.status_code,
-                name="HTTP Status Code",
-                attachment_type=allure.attachment_type.TEXT,
-            )
             allure.attach(
                 body_text, name="Response Body", attachment_type=attachment_type
+            )
+            allure.attach(
+                body=curlify.to_curl(response.request),
+                name="cURL Command",
+                attachment_type=allure.attachment_type.TEXT,
             )
 
     def _request(self, method, endpoint, **kwargs):
